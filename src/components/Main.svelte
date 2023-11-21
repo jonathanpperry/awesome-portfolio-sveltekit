52e1ded9184cc7bd05c049e15abb0f82748b247e
Jonathan Perry <jpper@umich.edu>
2023-11-21 17:29:57 -0500

    Implemented about section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,6 +6,24 @@
     { name: "Ultimate Todos", icon: "fa-solid fa-list-check" },
     { name: "Pokedex", icon: "fa-solid fa-diagram-project" },
   ];
+
+  let benefits = [
+    {
+      name: "a graduate of a top public university ",
+      description:
+        "I studied computer science at the University of Michigan College of Engineering. Some of my major electives were Artificial Intelligence, Computer Security, and User Interface Design.",
+    },
+    {
+      name: "a product design & UX fanatic",
+      description:
+        "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.",
+    },
+    {
+      name: "an excellent communicator",
+      description:
+        "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+    },
+  ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -92,5 +110,110 @@
         </p>
       </Step>
     </div>
+  </section>
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="poppins text-violet-400">about</span> me.
+      </h3>
+    </div>
+    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="poppings text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+          >
+            0{index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+
+    <!-- Table Section -->
+    <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">
+      The <span class="text-violet-400 poppins">Complete</span> Package
+    </h5>
+    <div
+      class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    >
+      <table class="bg-white text-slate-700 rounded text-center">
+        <thead class="border-b border-solid border-slate-200">
+          <tr class="">
+            <th />
+            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+            <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8"
+              >Me</th
+            >
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-4 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Dedication
+            </td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-4 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Critical Thought
+            </td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-4 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Interpersonal Skills
+            </td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-4 py-4 font-semibold text-sm whitespace-nowrap"
+            >
+              Programming Ability
+            </td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+      <p>Scroll to see more &rarr;</p>
+    </div>
+    <p class="mx-auto">So why not invest?</p>
   </section>
 </main>
